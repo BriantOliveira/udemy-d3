fa1e9859d84d5b3beb714948e441170ef576eedb
Briant Oliveira <briantmoliveira@gmail.com>
2019-01-29 13:07:24 -0600

    Adding a rectagle on activity 2.4

--- 02/2.04/js/main.js
+++ 02/2.04/js/main.js
@@ -8,8 +8,15 @@ var svg = d3.select("#chart-area").append("svg")
 	.attr("width", 400)
 	.attr("height", 400);
 
-var circle = svg.append("circle")
-	.attr("cx", 100)
-	.attr("cy", 250)
-	.attr("r", 70)
-	.attr("fill", "grey");
+// var circle = svg.append("circle")
+// 	.attr("cx", 100)
+// 	.attr("cy", 250)
+// 	.attr("r", 70)
+// 	.attr("fill", "grey");
+
+var rectangle = svg.append("rect")
+	.attr("x", 10)
+	.attr("y", 10)
+	.attr("width", 200)
+	.attr("height", 100)
+	.append("fill", "grey");
